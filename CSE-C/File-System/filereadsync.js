9badd1aee7086cc9ff92aeb171552dab472eb3be
Mukulkumar25 <mukul25164707@gmail.com>
2025-02-11 10:10:45 +0530

    updated data 11/02/2025

--- CSE-C/File-System/filereadsync.js
+++ CSE-C/File-System/filereadsync.js
@@ -1,3 +1,17 @@
 const fs = require("fs");
-const data = fs.readFileSync("./data.txt");
+// fs.writeFileSync("./data1.txt", "Hello ABES", "utf-8");
+// fs.writeFileSync("./data1.txt", "Hello ABESEC", "utf-8");
+// fs.appendFileSync("./data1.txt", " Student", "utf-8");
+// // fs.renameSync("./data1.txt", "./data3.txt");
+// fs.unlinkSync("./data3.txt");
+
+const data = fs.readFileSync("./data.txt", "utf-8");
 console.log(data);
+if(data.match("H")){
+  console.log("file contains H");
+  const newdata = data.replace("H", "ABES");
+  fs.writeFileSync("./data.txt", newdata, "utf-8");
+  
+}
+
+// console.log(data.toString());
